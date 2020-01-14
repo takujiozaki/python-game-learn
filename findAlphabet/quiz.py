b4f6660266bc0e4f51f8118eb843588d89620b34
takujiozaki <takujiozaki@gmail.com>
2020-01-14 13:47:25 +0900

    オブジェクトで書き直し

--- findAlphabet/quiz.py
+++ findAlphabet/quiz.py
@@ -6,43 +6,73 @@
 import random
 import datetime
 
-from findAlphabet import IO
+#入出力
+class SystemIO:
+	def print(self, word):
+		print(word)
 
-# 出題
+	def input(self):
+		return(input())
+
+#game flow
+class FindLetter:
+	#プロパティ
+	QUESTION = ''
+	r = ''
+	answer = ''
+	playtime = []
+
+	def __init__(self):
+		#問題文
+		self.QUESTION = [
+			'A','B','C','D','E','F','G',
+			'H','I','J','L','M','N','O',
+			'P','Q','R','S','T','V','W',
+			'X','Y','Z'
+		]
+		#正解
+		self.r = random.choice(self.QUESTION)
+
+	def description(self):
+		#説明
+		q_str = 'アルファベットがランダムに並んでいますが、一文字欠けています。\n欠けている文字はなんでしょうか？\n'
+		#問題文生成
+		random.shuffle(self.QUESTION)
+
+		for i in self.QUESTION:
+			if i != self.r:
+				q_str = q_str + i
+
+		return q_str
+
+	def your_answer(self, answer):
+		self.answer = answer
+
+	def judge(self):
+		judgement = ''
+		if self.answer == self.r:
+			judgement = '正解！ 解答にかかった時間は{}秒でした'.format(self.get_diff_time())
+		else:
+			judgement = '残念! 正解は{}でした'.format(self.r)
+		return judgement
+	
+	def set_play_time(self):
+		self.playtime.append(datetime.datetime.now())
+
+	def get_diff_time(self):
+		playtime = (self.playtime[1] - self.playtime[0]).seconds
+		return playtime 
 
 
-# 解答
-
-
-# 判定
-
-
-QUESTION = [
-	'A','B','C','D','E','F','G',
-	'H','I','J','L','M','N','O',
-	'P','Q','R','S','T','V','W',
-	'X','Y','Z'
-	]
-random.shuffle(QUESTION)
-q_str = ''
-r = random.choice(QUESTION)
-for i in QUESTION:
-	if i != r:
-		q_str = q_str + i
-		
-io = IO.IO()
-
-io.output(q_str)
-io.output('欠けている文字はなんでしょう？')
-start = datetime.datetime.now()
-
-answer=io.input()
-
-if answer == r:
-	end = datetime.datetime.now()
-	diff = (end - start).seconds
-	io.output('正解')
-	io.output('回答にかかった時間は{}秒でした'.format(str(diff)))
-else:
-	io.output('残念！')
-	io.output('正解は{}です'.format(r))
+game = FindLetter()
+io = SystemIO()
+#問題出力
+io.print(game.description())
+#開始時間計測
+game.set_play_time()
+#解答入力
+game.your_answer(io.input())
+#終了時間計測
+game.set_play_time()
+#判定
+io.print(game.judge())
