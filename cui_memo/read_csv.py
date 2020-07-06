4bd76400fb1296d61c3dbd1521747eebc98fd6bd
TakujiOzaki <takujiozaki@gmail.com>
2020-07-07 08:55:39 +0900

    文字コード指定追記

--- cui_memo/read_csv.py
+++ cui_memo/read_csv.py
@@ -2,15 +2,9 @@ import csv
 
 # csvモジュールで読み込む
 # withをつけて開くと閉じるは自動で行われる
-with open("memo.csv") as f:
+with open("memo.csv",encoding='utf-8') as f:
   for row in csv.reader(f):
     print(row)
     print(type(row))
 
-# 書き込みにもcsvモジュールを使用
-# open の引数はファイル名とモード(aが追記、wが書き換え)
-# 引数はリスト
 
-with open("memo.csv", "a") as f:
-  writer = csv.writer(f)
-  writer.writerow(['2020-07-07','七夕まつり'])
